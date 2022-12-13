1f0453ad256468731974408f600cca16c6a97d93
Bruno Hermes <48295386+brunohermes@users.noreply.github.com>
2022-12-13 10:29:06 -0300

    Vehicle Script
    Starting to code the vehicle logic

---
+++ Vehicle.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class Vehicle : MonoBehaviour
+{
+    #region Primitives
+    private WheelCollider[] wheels; //Sets up an array for the vehicle wheels
+    
+    [Header("Engine - Management")]
+	[Space(15)]
+    public bool engineOn;
+    #endregion
+  
+  
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
