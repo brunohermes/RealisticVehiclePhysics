cc2ad88cc1604f93ca3839127a8767f50510ddcd
Bruno Hermes <48295386+brunohermes@users.noreply.github.com>
2022-12-16 02:21:47 -0300

    New custom inspector
    Editor

--- VehicleEditor.cs
+++ VehicleEditor.cs
@@ -6,11 +6,18 @@ using UnityEngine;
 [CustomEditor(typeof(Vehicle))]
 public class VehicleEditor : Editor
 {
+   public Texture logo;
+ 
  public override void OnInspectorGUI(){
-    Vehicle vehicleScript = (Vehicle)target;
-    EditorGUILayout.FloatField("Steering Angle", vehicleScript.steeringAngle);
-    EditorGUILayout.HelpBox("Set how much your vehicle will Steer", MessageType.Info);
- 
- 
+  
+   GUI.DrawTexture(new Rect(10, 0, 100, 100), logo, ScaleMode.StretchToFill, true, 10.0f);
+   GUILayout.Space(120);
+   EditorGUILayout.LabelField("Movement Settings", EditorStyles.boldLabel);
+   GUILayout.Space(50);
+   Vehicle vehicleScript = (Vehicle)target;
+   EditorGUILayout.FloatField("Steering Angle", vehicleScript.steeringAngle);
+   EditorGUILayout.HelpBox("Set how much your vehicle will Steer", MessageType.Info);
+  
  }   
+
 }
